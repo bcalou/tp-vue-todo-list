582df76e467977d02ce92df19d20e4e8355fa4e1
bcalou <bastien.calou@gmail.com>
2019-09-25 09:44:11 +0200

    [feat] display list of todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,39 @@
 <template>
   <main>
-    Todo List
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.done" :id="todo.id" />
+        <label :for="todo.id">{{ todo.name }}</label>
+      </li>
+    </ul>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: 'Manger',
+          done: true,
+        },
+        {
+          id: 2,
+          name: 'Boire',
+          done: false,
+        },
+      ],
+    };
+  },
+});
 </script>
 
 <style scoped lang="scss">
+input:checked ~ label {
+  text-decoration: line-through;
+}
 </style>
