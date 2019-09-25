f60e170b3624d855de005dcd2911d5574ba01ca5
bcalou <bastien.calou@gmail.com>
2019-09-25 10:22:28 +0200

    [refacto] todo-item child component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <input type="checkbox" v-model="todo.done" :id="todo.id" />
+    <label :for="todo.id">{{ todo.name }}</label>
+  </li>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Todo from '@/models/todo';
+
+export default Vue.extend({
+  props: {
+    todo: Object as () => Todo,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+input:checked ~ label {
+  text-decoration: line-through;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,10 +3,7 @@
     <todo-new v-on:newTodo="addTodo($event)"></todo-new>
 
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <input type="checkbox" v-model="todo.done" :id="todo.id" />
-        <label :for="todo.id">{{ todo.name }}</label>
-      </li>
+      <todo-item v-for="todo in todos" :key="todo.id" :todo="todo"></todo-item>
     </ul>
 
     <button
@@ -23,10 +20,12 @@
 import Vue from 'vue';
 import Todo from '@/models/todo';
 import TodoNew from '@/components/TodoNew.vue';
+import TodoItem from '@/components/TodoItem.vue';
 
 export default Vue.extend({
   components: {
     TodoNew,
+    TodoItem,
   },
   data(): { todos: Todo[] } {
     return {
@@ -61,7 +60,4 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-input:checked ~ label {
-  text-decoration: line-through;
-}
 </style>
