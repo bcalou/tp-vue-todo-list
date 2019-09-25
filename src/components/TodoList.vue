04cd8ac5e0d587d52d813f7d37f1653e742a6643
bcalou <bastien.calou@gmail.com>
2019-09-25 09:36:01 +0200

    [feat] add todo-list component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    Todo List
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <header>
+      <h1>Todo list</h1>
+    </header>
+
+    <todo-list></todo-list>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import TodoList from '@/components/TodoList.vue';
 
 export default Vue.extend({
   name: 'home',
-  components: {},
+  components: {
+    TodoList,
+  },
 });
 </script>
