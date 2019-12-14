122f6b2fbaaf3b7ba65af6e5c2e153b4318abd84
bcalou <bastien.calou@gmail.com>
2019-12-14 19:15:46 +0100

    implement required field error without composition api

--- src/components/TodoItem/TodoItem.vue
+++ src/components/TodoItem/TodoItem.vue
@@ -2,14 +2,13 @@
   <li class="todoItem">
     <input type="checkbox" v-model="todo.done" class="todoItem__checkbox" />
 
-    <form
-      @submit.prevent="setName()"
-      v-if="todo.editing"
-      class="todoItem__edit"
-    >
-      <input v-model="newTodoName" ref="newNameInput" />
-      <button>Edit</button>
-    </form>
+    <div v-if="todo.editing">
+      <form @submit.prevent="setName()" class="todoItem__edit">
+        <input v-model="newTodoName" ref="newNameInput" />
+        <button>Edit</button>
+      </form>
+      <p v-if="inputError" class="error">The todo name cannot be empty</p>
+    </div>
 
     <div v-else @click="enterEditMode()" class="todoItem__name">
       {{ todo.name }}
@@ -25,9 +24,10 @@ export default Vue.extend({
   props: {
     todo: Object as () => Todo,
   },
-  data(): { newTodoName: string } {
+  data(): { newTodoName: string; inputError: boolean } {
     return {
       newTodoName: this.todo.name,
+      inputError: false,
     };
   },
   watch: {
@@ -36,6 +36,11 @@ export default Vue.extend({
         todo: this.todo,
         done: newValue,
       });
+    },
+    'newTodoName'(newTodoName) {
+      if (newTodoName.length) {
+        this.inputError = false;
+      }
     },
   },
   methods: {
@@ -49,13 +54,14 @@ export default Vue.extend({
       }
     },
     setName(): void {
-      this.$store.dispatch('todos/quitEditMode');
-
       if (this.newTodoName.length) {
         this.$store.dispatch('todos/setName', {
           todo: this.todo,
           name: this.newTodoName,
         });
+        this.$store.dispatch('todos/quitEditMode');
+      } else {
+        this.inputError = true;
       }
     },
   },
--- src/components/TodoNew/TodoNew.vue
+++ src/components/TodoNew/TodoNew.vue
@@ -1,21 +1,25 @@
 <template>
-  <form @submit.prevent="addTodo()" class="todoNew">
-    <input
-      placeholder="Do something..."
-      v-model="newTodoName"
-      class="todoNew__input"
-    />
-    <button class="todoNew__addButton">Add</button>
-  </form>
+  <div>
+    <form @submit.prevent="addTodo()" class="todoNew">
+      <input
+        placeholder="Do something..."
+        v-model="newTodoName"
+        class="todoNew__input"
+      />
+      <button class="todoNew__addButton">Add</button>
+    </form>
+    <p v-if="inputError" class="error">The todo name cannot be empty</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
-  data(): { newTodoName: string } {
+  data(): { newTodoName: string; inputError: boolean } {
     return {
       newTodoName: '',
+      inputError: false,
     };
   },
   methods: {
@@ -24,6 +28,15 @@ export default Vue.extend({
         this.$store.dispatch('todos/add', { name: this.newTodoName });
 
         this.newTodoName = '';
+      } else {
+        this.inputError = true;
+      }
+    },
+  },
+  watch: {
+    newTodoName(newTodoName) {
+      if (newTodoName.length) {
+        this.inputError = false;
       }
     },
   },
